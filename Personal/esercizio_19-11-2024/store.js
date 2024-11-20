11381112c9f9d05e5b8e8777df03ab7b347271e9
Marco <Filanninobusiness@gmail.com>
2024-11-20 17:41:27 +0100

    debug

--- Personal/esercizio_19-11-2024/store.js
+++ Personal/esercizio_19-11-2024/store.js
@@ -1,127 +1,155 @@
+const griglia = document.getElementById("contenitore-prodotti");
+const select = document.getElementById("contenitore-categorie");
+const prezzoMinimo = document.getElementById("prezzo-minimo");
+const prezzoMassimo = document.getElementById("prezzo-massimo");
+const resetB = document.getElementById("reset");
 
-
-const categoria = []
 async function prodotti(url) {
-    
-    try {
-        
+  try {
+    const response = await fetch(url);
 
-        const response = await fetch(url)
-        const data = await response.json()
-        const griglia = document.getElementById("contenitore-prodotti")
-        const nav = document.querySelector("nav")
-       
-
-        
-
-        if(!response.ok){
-            console.error(error)
-            return
-        }
-
-         
-
-        data.forEach(element => 
-            
-{
-    console.log(element)
-   
-
-           //const tutto = tutteCategorie(element)
-           // aside.appendChild(tutto)//
-            
-            const prodotti = creacard(element)
-            griglia.appendChild(prodotti)
-        
-        }
-            
-    );        
-    } catch (error) {
-        console.error(error);
+    if (!response.ok) {
+      console.error(`Errore nella fetch: ${response.statusText}`);
+      return;
     }
+
+    const data = await response.json();
+
+    // Trova gli elementi del DOM
+
+    // Controlla se gli elementi sono stati trovati
+    if (!griglia) {
+      console.error("Elemento 'contenitore-prodotti' non trovato");
+      return;
+    }
+    if (!select) {
+      console.error("Elemento 'contenitore-categorie' non trovato");
+      return;
+    }
+
+    const categoria = [];
+
+    //opzione all
+    const optionAll = document.createElement("option");
+    optionAll.value = "all";
+    optionAll.innerText = "All";
+    select.appendChild(optionAll);
+
+    // Popola la select e aggiungi tutte le opzioni uniche
+    data.forEach((element) => {
+      if (!categoria.includes(element.category)) {
+        categoria.push(element.category);
+
+        const option = document.createElement("option");
+        option.value = element.category;
+        option.innerText = element.category;
+        select.appendChild(option);
+      }
+    });
+
+    // Aggiungi tutte le carte dei prodotti alla griglia
+    mostraProdotti(data, griglia);
+
+    // Aggiungi l'evento per filtrare le categorie
+    //si attiva quando l'utente cambia l'opzione selezionata del menu
+    select.addEventListener("change", () => filtra(data));
+
+    prezzoMinimo.addEventListener("input", () => filtra(data));
+
+    prezzoMassimo.addEventListener("input", () => filtra(data));
+
+    //pulsante reset
+
+    resetB.addEventListener("click", () => {
+      griglia.innerHTML = "";
+
+      data.forEach((prodotto) => {
+        const card = creaCard(prodotto);
+        griglia.appendChild(card);
+      });
+    });
+
+    //fine pulsante reset
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-
-   
-   prodotti("https://fakestoreapi.com/products")
-
-function creacard(obj) {
-    const div = document.createElement("div")
-    const img = document.createElement("img")
-    const titolo = document.createElement("h3")
-    const descrizione = document.createElement("p")
-    const prezzo = document.createElement("p")
-    const acquista = document.createElement("button")
-    const preferiti = document.createElement("button")
-    
-
-    div.classList.add("box")
-    img.classList.add("box-img")
-    acquista.classList.add("bottone-acquisti")
-    preferiti.classList.add("bottone-pref")
-    prezzo.classList.add("prezzo")
-    titolo.classList.add("titolo")
-    descrizione.classList.add("descrizione")
-    
-
-    img.src = obj.image
-    img.alt = obj.title
-    titolo.innerText = obj.title
-    descrizione.innerText = obj.description
-    prezzo.innerText = obj.price
-    acquista.innerText = "🛒Acquista"
-    preferiti.innerText = "❤️Preferiti"
-    
-    
-
-    div.appendChild(img)
-    div.appendChild(titolo)
-    div.appendChild(descrizione)
-    div.appendChild(prezzo)
-    div.appendChild(acquista)
-    div.appendChild(preferiti)
-
-   
-    
-
-    return div
-
+// Funzione per mostrare i prodotti nella griglia
+function mostraProdotti(prodotti, griglia) {
+  griglia.innerHTML = ""; // Svuota la griglia prima di aggiungere nuovi elementi
+  prodotti.forEach((prodotto) => {
+    const card = creaCard(prodotto);
+    griglia.appendChild(card);
+  });
 }
 
-const select = document.querySelector("#contenitore-categorie")
-console.log(categoria[2])
-console.log(select)
+function filtra(prodotti) {
+  griglia.innerHTML = "";
+  const minimoValue = prezzoMinimo.value;
+  const massimoValue = prezzoMassimo.value;
+  const categoriaSelezionata = select.value;
 
+  const datiFiltrati = prodotti.filter((prodotto) => {
+    let prezzo = Math.ceil(prodotto.price);
 
+    const filtroCategoria =
+      categoriaSelezionata === "all" ||
+      prodotto.category === categoriaSelezionata;
 
-const optionDue = document.createElement("option")
+    const filtroPrezzo = prezzo >= minimoValue && prezzo <= massimoValue;
 
-optionDue.value = "element.category"
+    return filtroCategoria && filtroPrezzo;
+  });
 
-optionDue.innerText = "element.category"
+  datiFiltrati.forEach((prodotto) => {
+    const card = creaCard(prodotto);
+    griglia.appendChild(card);
+  });
+}
 
-select.appendChild(optionDue)
+function creaCard(obj) {
+  const div = document.createElement("div");
+  const divImg = document.createElement("div");
+  const img = document.createElement("img");
+  const divTesto = document.createElement("div");
+  const titolo = document.createElement("h3");
+  const descrizione = document.createElement("p");
+  const prezzo = document.createElement("p");
+  const divBtn = document.createElement("div");
+  const acquista = document.createElement("button");
+  const preferiti = document.createElement("button");
 
-console.log(optionDue)
+  div.classList.add("box");
+  divImg.classList.add("div-img");
+  img.classList.add("box-img");
+  divTesto.classList.add("div-testo");
+  divBtn.classList.add("div-btn");
+  acquista.classList.add("bottone-acquisti");
+  preferiti.classList.add("bottone-pref");
+  prezzo.classList.add("prezzo");
+  titolo.classList.add("titolo");
+  descrizione.classList.add("descrizione");
 
+  img.src = obj.image;
+  img.alt = obj.title;
+  titolo.innerText = obj.title;
+  descrizione.innerText = obj.description;
+  prezzo.innerText = `€ ${obj.price}`;
+  acquista.innerText = "🛒 Acquista";
+  preferiti.innerText = "❤️ Preferiti";
 
+  divImg.appendChild(img);
+  div.appendChild(divImg);
+  div.appendChild(divTesto);
+  divTesto.appendChild(titolo);
+  divTesto.appendChild(descrizione);
+  divTesto.appendChild(prezzo);
+  divBtn.appendChild(acquista);
+  divBtn.appendChild(preferiti);
+  div.appendChild(divBtn);
 
+  return div;
+}
 
-
-categoria.forEach(element => {
-
-    const option = document.createElement("option")
-
-    const optValue = option.value
-
-
-    optValue = element.category
-
-    optValue.innerText = element.category
-
-    select.appendChild(option)
-
-    console.log(option)
-})
-
-
+prodotti("https://fakestoreapi.com/products");
